cc4ff6758c81fe79e6594e9620cc90c5ae6e8fcc
JamaalPogue <jspogue@gmail.com>
2023-09-12 19:20:32 -0500

    Add code from textbook

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -1,4 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("I just added this in Visual Studio");
-Console.WriteLine("I just added this in the command line repo");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("I just added this in Visual Studio");
+        Console.WriteLine("I just added this in the command line repo");
+    }
+}
