1b3180ab71505a2a309b2a2c8111be1a5bfa04ef
JamaalPogue <jspogue@gmail.com>
2023-09-25 21:34:41 -0500

    Add properties

---
+++ ProGitForProgrammers/Book.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProGitForProgrammers
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public List<stirng> Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+    }
+}
+
